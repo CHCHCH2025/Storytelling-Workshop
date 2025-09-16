4466ef9ce1bf17aec08569bc8cd9407d11478ed1
CHCHCH2025 <christoph.hienerth@whu.edu>
2025-09-16 21:19:48 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,8 @@
-// App.js will be added next
+export default function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>🎉 Hello from Storytelling Workshop!</h1>
+      <p>Your app is working! Now we can bring back your real logic.</p>
+    </div>
+  );
+}
